85bd62243ff7499da55b838495fd275bd84ca95e
Leon Windheuser <leon.windheuser@gmail.com>
2022-12-19 13:34:05 +0100

    Add nix shell

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+    nativeBuildInputs = with pkgs; [
+        gcc 
+        bison
+        flex
+        ninja
+        clang-tools
+        cmake
+        gtest
+        fmt
+        linuxKernel.packages.linux_5_18.perf
+        valgrind
+        tbb
+    ];
+}
